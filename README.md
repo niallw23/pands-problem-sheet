17cf7db0fe7f81900f6e65e00d024d2e3b7425e1
Niall Wynne <nwynne93@gmail.com>
2023-02-02 23:45:59 +0000

    Task 02 - bank.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pands-problem-sheet
 ### Problem Sheet Tasks
 Task 01 contains helloworld.py which reads 'Hello World!'
+
+Task 02 is bank.py
---
+++ bank.py
@@ -0,0 +1,17 @@
+# bank.py
+# Author: Niall Wynne
+# Week 2 problem
+
+number1 = int(input('Enter first amount in cent: '))
+print(f'Amount = {number1}')
+
+number2 = int(input('Enter second amount in cent: '))
+print(f'Amount = {number2}')
+
+total = number1 + number2
+
+print(f'The sum of these is {total}')
+
+print('The sum of these is €2.45')
+
+# Need to research how to have the total display in euro
