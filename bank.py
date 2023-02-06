2141912e9376ab50be8a0da75d5611cad230e8db
Niall Wynne <nwynne93@gmail.com>
2023-02-06 22:45:47 +0000

    Updated Task 02 - bank.py

--- bank.py
+++ bank.py
@@ -8,10 +8,16 @@ print(f'Amount = {number1}')
 number2 = int(input('Enter second amount in cent: '))
 print(f'Amount = {number2}')
 
-total = number1 + number2
+total = number1/100 + number2/100    
 
-print(f'The sum of these is {total}')
+# print(f'The sum of these is {total}')
 
-print('The sum of these is €2.45')
+# print('The sum of these is €2.45') Initial attempt.
 
-# Need to research how to have the total display in euro
+# Left in the code from my initial attempts so I could see the difference.
+
+txt = "The sum of these is €{:.2f}"
+print(txt.format(total))
+
+
+# added the last two lines after finishing the lab and following the link to the Python tutorial.
