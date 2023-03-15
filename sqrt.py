d0120f3dde639e18bc6bc2fcd03de009c98d3782
Niall Wynne <nwynne93@gmail.com>
2023-03-15 12:43:50 +0000

    Task 06

---
+++ sqrt.py
@@ -0,0 +1,21 @@
+# sqrt.py
+# Author - Niall Wynne
+# A program that takes a positive floating-point number as input 
+# and outputs an approximation of its square root.
+# Need to create a function
+# Week 6 Task
+
+num = float(input('Enter a positive floating point number:'))
+
+
+
+def sqrt(num):
+    approx = num * 0.5
+    better = 0.5 * (approx + num / approx)
+    while better != approx:
+        approx = better
+        better = 0.5 * (approx + num /approx)
+    return approx
+
+print (f'The square root of {num} is approx. {sqrt(num)}')
+
