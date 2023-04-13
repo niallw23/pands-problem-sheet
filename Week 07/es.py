c18346c56f31308a4522efd8159be6146da68db4
Niall Wynne <nwynne93@gmail.com>
2023-04-13 13:14:50 +0100

    Update to Week07 following feedback

--- Week 07/es.py
+++ Week 07/es.py
@@ -7,13 +7,22 @@
 import sys  # looked up when researching command line
 
 arg = sys.argv[1] # skipping the python file name
-
+'''
 if arg == 'note.txt': # assuming file name is known and can be input into command line
     def letterFrequency(fileName, letter):
         file = open(fileName, 'r')
         text = file.read()
         return text.count(letter)
- 
- 
+'''
+# following feedback, tried to use 'with' statement instead of file = open()
+# never closed file in above attempt
+# the reading on with statements in the Real Python explained this further
+
+def letterFrequency(fileName, letter):  #still making same assumption around file name
+    with open(fileName, 'r') as file:
+        text = file.read()
+        return text.count(letter)
+
+
 # call the function and display the letter count
 print(letterFrequency('note.txt', 'e'))
